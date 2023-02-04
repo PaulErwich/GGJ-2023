f5ea337e7c008a694e923dc8215b4961151357c0
Luryann <luryann2.augustocervi@live.uwe.ac.uk>
2023-02-04 15:38:20 +0000

    Fixed flying animation

--- Assets/Scripts/MessageHandler.cs
+++ Assets/Scripts/MessageHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MessageHandler : MonoBehaviour
@@ -8,24 +9,66 @@ public class MessageHandler : MonoBehaviour
 
     [SerializeField]private GameObject _messagePrefab; // Reference to the message prefab
     private Animator _animator;
+    private GameObject _canvas;
+    private bool _canvasFound = false;
 
+    // Message header
+    private GameObject _headerGameObject;
+    private TextMeshProUGUI _headerText;
+    
+    // Message description
+    private GameObject _descriptionGameObject;
+    private TextMeshProUGUI _descriptionText;
+    
     private void Awake()
     {
-        _animator = _messagePrefab.gameObject.GetComponent<Animator>();
+        _headerGameObject = GameObject.Find("Header"); // Find header GameObject
+        _descriptionGameObject = GameObject.Find("Description"); // Find description GameObject
+    }
+
+    private void Start()
+    {
+        _headerText = _headerGameObject.GetComponent<TextMeshProUGUI>();
+        _descriptionText = _descriptionGameObject.GetComponent<TextMeshProUGUI>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            CreateMessage();
+            _animator = GameObject.FindGameObjectWithTag("Message").GetComponent<Animator>();
+            _animator.SetBool("ShowMessage", true);
         }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StartCoroutine(UpdateMessageCoroutine("T-Rex", "This is the description"));
+        }
+
+        if (!GameObject.FindGameObjectWithTag("Canvas") && !_canvasFound) return;
+        _canvas = GameObject.FindGameObjectWithTag("Canvas");
+        _canvasFound = true;
     }
 
     public void CreateMessage()
     {
-        Instantiate(_messagePrefab, GameObject.FindGameObjectWithTag("Canvas").transform);
+        Instantiate(_messagePrefab, Vector3.zero, Quaternion.identity, _canvas.transform);
     }
-    
+
+    public void UpdateMessageContent(string header, string description)
+    {
+        _headerText.text = header;
+        _descriptionText.text = description;
+    }
+
+
+    IEnumerator UpdateMessageCoroutine(string header, string description)
+    {
+        _animator.SetBool("ShowMessage", false);
+        yield return new WaitForSeconds(1.2f);
+        UpdateMessageContent(header, description);
+        _animator.SetBool("ShowMessage", true);
+    }
 }
