6bf0c33b647d9b3be1354308fff0148222e367f1
Nathan Scott <Nathan4.Scott@live.uwe.ac.uk>
2023-02-05 11:53:49 +0000

    Main menu kind of done
    Its music works too

--- Assets/Scripts/UI & Music/Music.cs
+++ Assets/Scripts/UI & Music/Music.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Music : MonoBehaviour
 {
     private AudioSource audio;
-    
+    private bool began;
     void Start()
     {
         audio = gameObject.GetComponent<AudioSource>();
+        Time.timeScale = 0;
+        began = false;
     }
-    // Update is called once per frame
-    void Begin()
+    void Update()
     {
-        audio.clip = Resources.Load<AudioClip>("Audio/Main_theme_C");
-        audio.Play();
+        if (Input.anyKey && began == false)
+        {
+            audio.clip = Resources.Load<AudioClip>("Audio/Main_theme_C");
+            audio.Play();
+            Time.timeScale = 1;
+            gameObject.transform.Find("Title").gameObject.SetActive(false);
+            began = true;
+        }
     }
 
     void Deeper()
